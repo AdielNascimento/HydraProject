4ef0d8b1b05c5058f02a1b473d18aa6ab6d7a652
Adiel Adiel <adieltab16@gmail.com>
2019-11-22 12:32:44 +0000

    primeiro

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+
+from keras.layers import Input
+from keras.models import Model, Sequential
+from keras.layers.core import Dense, Dropout
+from keras.layers.advanced_activations import LeakyReLU
+from keras.datasets import mnist
+from keras.optimizers import Adam
+from keras import initializers
